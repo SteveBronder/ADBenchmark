088cf7a22dc48ccd445c89f6df97311e007788b1
Steve Bronder <sbronder@flatironinstitute.org>
2025-08-27 17:30:50 -0400

    fix adolc

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,16 @@ cmake --build ./build --target adolc_sum -j8 -v
 ./build/benchmark/adolc/adolc_sum 
 
 cmake --build ./build --target cppad_sum -j8 -v
-./build/benchmark/adolc/cppad_sum 
+./build/benchmark/cppad/adept_sum 
 
-cmake --build ./build --target fastad_sum -j8 -v
-./build/benchmark/adolc/fastad_sum 
+cmake --build ./build --target cppad_sum -j8 -v
+./build/benchmark/cppad/adept_sum 
 
-cmake --build ./build --target sacado_sum -j8 -v
-./build/benchmark/adolc/sacado_sum 
+#cmake --build ./build --target fastad_sum -j8 -v
+#./build/benchmark/fastad/fastad_sum 
 
-cmake --build ./build --target stan_sum -j8 -v
-./build/benchmark/adolc/stan_sum 
+#cmake --build ./build --target sacado_sum -j8 -v
+#./build/benchmark/sacado/sacado_sum 
+
+#cmake --build ./build --target stan_sum -j8 -v
+#./build/benchmark/stan/stan_sum 
